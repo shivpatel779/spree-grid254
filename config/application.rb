08ed301fa1f625fa2a7839bd0fc74ae6ad7c72e1
Kushal <ror@skywidesys.com>
2016-08-10 13:11:18 +0530

    new layout integration

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module SpreeDemo
       end
     end
 
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
