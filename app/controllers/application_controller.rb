82e7fdcae4908fe65865ce4f594947bdccfcc656
skywidesys <ror@skywidesys.com>
2016-10-05 16:09:19 +0530

    modified load values for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :create_user_wallet_if_missing
+  before_filter :initialize_user_data
 
-  def create_user_wallet_if_missing
+  def initialize_user_data
     if spree_current_user
       spree_current_user.create_wallet if spree_current_user.wallet.nil?
+      spree_current_user.create_referral_credit if spree_current_user.spree_referral_credit.nil?
     end
   end
 
