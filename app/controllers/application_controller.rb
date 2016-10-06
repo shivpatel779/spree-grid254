df9764baf86fd0df390582754e7da5671337d811
skywidesys <ror@skywidesys.com>
2016-10-06 20:11:12 +0530

    for existing users, generated referral code if they don't have any

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,9 @@ class ApplicationController < ActionController::Base
     if spree_current_user
       spree_current_user.create_wallet if spree_current_user.wallet.nil?
       spree_current_user.create_referral_credit if spree_current_user.spree_referral_credit.nil?
+      if spree_current_user.referral_code == ''
+        spree_current_user.update_attributes(referral_code: referral_code = (0...8).map { (65 + rand(26)).chr }.join)
+      end
     end
   end
 
