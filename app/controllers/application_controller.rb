681fbbdf60baef2709ba510984a9bab5d4cfae65
skywidesys <ror@skywidesys.com>
2016-10-06 20:12:02 +0530

    resolved typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
       spree_current_user.create_wallet if spree_current_user.wallet.nil?
       spree_current_user.create_referral_credit if spree_current_user.spree_referral_credit.nil?
       if spree_current_user.referral_code == ''
-        spree_current_user.update_attributes(referral_code: referral_code = (0...8).map { (65 + rand(26)).chr }.join)
+        spree_current_user.update_attributes(referral_code: ((0...8).map { (65 + rand(26)).chr }.join))
       end
     end
   end
