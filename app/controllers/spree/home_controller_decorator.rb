fa760e38f963e073446647a2736b36ddf1fa652d
shiv <shiv.yuvasoft@gmail.com>
2017-01-05 13:46:18 +0530

    update index page query

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -5,7 +5,7 @@ module Spree
     end
 
     def index
-    	@treding_deal_products = Spree::Product.where.not(total_discount:nil).order( 'total_discount desc')
+    	@treding_deal_products = Spree::Product.where.not("total_discount = ? AND seller_id = ?",nil,"").order( 'total_discount desc')
     	@top_ending_deals_products = @treding_deal_products.where(discontinue_on: Date.today.strftime("%Y-%m-%d")+" 00:00:00"..Date.today.days_ago(-5).strftime("%Y-%m-%d")+" 00:00:00")
     	@products = Spree::Product.first(4)
     end
