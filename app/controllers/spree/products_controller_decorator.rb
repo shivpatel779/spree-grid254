b9edb2ecea8d7a91d8ead0ab284a136b6f391a97
shiv <shiv.yuvasoft@gmail.com>
2017-01-17 18:58:53 +0530

    worked on message

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -32,7 +32,8 @@ Spree::ProductsController.class_eval do
     @seller_ids = @product.seller_id.split(",").reject(&:blank?)
     @seller = Spree::Seller.find(@seller_ids)
     @store_ids = @seller.map(&:store_locations).flatten.collect(&:id)  
-    @store_locations = Spree::StockMovement.where(product_id:params[:id]).where(store_location_id:@store_ids)
+    # @store_locations = Spree::StockMovement.where(product_id:params[:id]).where(store_location_id:@store_ids)
+    @store_locations = Spree::StockMovement.where(product_id:params[:id])
   end
 
   def product_offer_list
