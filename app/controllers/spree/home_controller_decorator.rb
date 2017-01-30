a81b6d331d36778ff6be67a58d3c64eed8012f03
shiv <shiv.yuvasoft@gmail.com>
2017-01-30 17:19:57 +0530

    update discontinue

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -5,17 +5,18 @@ module Spree
     end
 
     def index
-      @treding_deal_products = Spree::Product.where.not("total_discount = ? AND seller_id = ?",nil,"").order( 'total_discount desc')
-    	@top_ending_deals_products = @treding_deal_products.where(discontinue_on: Date.today.strftime("%Y-%m-%d")+" 00:00:00"..Date.today.days_ago(-5).strftime("%Y-%m-%d")+" 00:00:00")
+      # @treding_deal_products = Spree::Product.where.not("total_discount = ? AND seller_id = ?",nil,"").order( 'total_discount desc')
+    	@treding_deal_products = Spree::Product.product_validity.where.not("total_discount = ? AND seller_id = ?",nil,"").order( 'total_discount desc')
+      @top_ending_deals_products = @treding_deal_products.where(discontinue_on: Date.today.strftime("%Y-%m-%d")+" 00:00:00"..Date.today.days_ago(-5).strftime("%Y-%m-%d")+" 00:00:00")
     	@products = Spree::Product.first(4)
       if spree_current_user
         if spree_current_user.product_favorites.present?
           @recommended = spree_current_user.product_favorites.collect{|p| p.product}.collect{|t| t.taxons}.flatten.map{|p| p.products}.flatten
         else
-          @recommended = Spree::Product.where.not( seller_id: "")
+          @recommended = Spree::Product.product_validity.where.not( seller_id: "")
         end
       else
-        @recommended = Spree::Product.where.not( seller_id: "")
+        @recommended = Spree::Product.product_validity.where.not( seller_id: "")
       end
     end
   end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,6 +2,10 @@ Spree::Product.class_eval do
   has_many :favorites, class_name: 'Spree::Favorite', foreign_key: :product_id, :dependent => :destroy
   has_many :stock_movements
   has_many :variants
+  
+  # scope 
+  scope :product_validity, -> { where(['discontinue_on >= ?', Date.today.strftime("%Y-%m-%d")+" 00:00:00"]) }
+
   def total_favorites
     favorites.count.to_s
   end
